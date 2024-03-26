a19cd1a18310f5754fb288cd4dbe5947025a3934
sebapi747 <sebapi747@github.com>
2024-03-26 16:41:08 +0800

    telegram

--- sec.py
+++ sec.py
@@ -7,9 +7,17 @@ import os
 import config
 outdir = config.dirname
 
+def sendTelegram(text):
+    prefix = os.uname()[1] + __file__ + ":"
+    params = {'chat_id': config.telegramchatid, 'text': prefix+text, 'parse_mode': 'HTML'}
+    resp = requests.post('https://api.telegram.org/bot{}/sendMessage'.format(config.telegramtoken), params)
+    resp.raise_for_status()
+
 url = "https://www.sec.gov/dera/data/financial-statement-data-sets.html"
 x = requests.get(url)
 print("%s %d" % (url,x.status_code))
+if x.status_code!=200:
+    sendTelegram("error %s %d" %(url,x.status_code))
 parsed_body=html.fromstring(x.text)
 
 href = parsed_body.xpath('//td[@class="views-field views-field-field-display-title"]/a/@href')
